48ec85db86b47d640766f2d67c6065f694243cbf
vicfriedman <victoria.m.friedman@gmail.com>
2016-01-26 10:51:59 -0500

    tests

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ## Objectives
 
-+ Effectively use anonymous function
++ Effectively use anonymous functions
 
 ## Intro
 
-You've been hired to work for an online Juke Box company. Your job is to clean up their messy code and to replace any defined functions that are only used once with an anonymous function.
+You've been hired to work for an online Juke Box company. Your job is to clean up their messy code and to replace any defined functions that are only used once with an anonymous function. 
 
 ## Instructions
 
@@ -17,7 +17,6 @@ click events that take in callback functions that are defined somewhere else - r
 should contain a fully functioning juke box ("hash of songs (key is artist - value is song") with the following features:
 
   + should be menu button (display options: play, pause, next, display songs)
-  + should be button to move forwards to next song `"now playing <song_name>"`
   + button to pause song - displays text `"<song_name> is paused"`
   + button to play song - displays text `"<song_name> by <artist_name> is now  playing"`
   + button to display songs - iterates over song "hash" and displays all songs and artist
---
+++ spec/jukebox_spec.js
@@ -0,0 +1,74 @@
+describe('Jukebox', function() {
+
+  beforeEach(function() {
+    setFixtures('<body><div><label for="menu" name="menu"><input type="button" id="menu" value="menu"><p id="menuOptions"></p></div><div><label for="play" name="play"><input type="button" id="play" value="play"><p id="songPlaying"></p></div><div><label for="pause" name="pause"><input type="button" id="pause" value="pause"><p id="songPaused"></p></div><div><label for="show" name="show"><input type="button" id="show" value="show"><p id="showSongs"></p></div></body>');
+  });
+
+  it('#menu binds a click event to the menu button', function() {
+    menu();
+    var menuButton = $('#menu')[0];
+    var menuClickEvent = $._data(menuButton, "events");
+    var myKey = [];
+    for (var key in menuClickEvent){ myKey.push(key);}
+    expect(myKey[0]).toEqual("click");
+  });
+
+  it('#menu shows the jukebox options in the browser', function() {
+    menu();
+    $('#menu').click();
+    expect($('#menuOptions').text()).toEqual("You can play a song, pause a song, or view all the songs");
+  });
+
+  it('#play adds a click event to the play button', function() {
+    play();
+    var playButton = $('#play')[0];
+    var playClickEvent = $._data(playButton, "events");
+    var myKey = [];
+    for (var key in playClickEvent){ myKey.push(key);}
+
+    expect(myKey[0]).toEqual("click");
+  });
+
+  it('#play displays the song being played', function() {
+    //failing
+    var songToPlay = "halo";
+    var theArtist = "beyonce";
+    play();
+    //#play undefined?
+    expect($('#songPlaying').text()).toEqual("now playing halo by beyonce");
+  });
+
+  it('#pause adds a click event to the pause button', function() {
+    pause();
+    var pauseButton = $('#pause')[0];
+    var pauseClickEvent = $._data(pauseButton, "events");
+    var myKey = [];
+    for (var key in pauseClickEvent){ myKey.push(key);}
+
+    expect(myKey[0]).toEqual("click");
+  });
+
+  it('#pause displays the song being paused', function() {
+    //failing
+    var songToPlay = "halo";
+    pause();
+    expect($('#songPaused').text()).toEqual("halo is paused");
+  });
+
+    it('#show adds a click event to the show button', function() {
+    show();
+    var showButton = $('#show')[0];
+    var showClickEvent = $._data(showButton, "events");
+    var myKey = [];
+    for (var key in showClickEvent){ myKey.push(key);}
+
+    expect(myKey[0]).toEqual("click");
+  });
+
+  it('#show displays the songs in the jukebox', function() {
+    //failing
+    show()
+    expect($('#showSongs').text()).toEqual("in the clear by taylor swift is available to play. halo by beyonce is available to play. hotline bling by drake is available to play.");
+  });
+
+});
