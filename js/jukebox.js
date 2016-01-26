0d6d201af8555ed2f5c381ab65bbc46f079050fb
vicfriedman <victoria.m.friedman@gmail.com>
2016-01-26 10:52:22 -0500

    code to refactor

---
+++ js/jukebox.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+  menu();
+  play();
+  pause();
+  show();
+
+
+});
+
+  var jukeboxSongs = { "taylor swift": "in the clear", "beyonce": "halo", "drake": "hotline bling"};
+  var artists = Object.keys(jukeboxSongs);
+  var theArtist = artists[Math.floor(Math.random()*artists.length)];
+  var songToPlay = jukeboxSongs[theArtist];
+
+
+  // var jukeboxSongs = { "taylor swift": "in the clear", "beyonce": "halo", "drake": "hotline bling"};
+  // var artists = Object.keys(jukeboxSongs);
+  // var theArtist = artists[Math.floor(Math.random()*artists.length)];
+  // var songToPlay = jukeboxSongs[theArtist];
+
+  function menu(){
+    $('#menu').on('click', menuOptions);
+  }
+
+   function menuOptions() {
+    $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
+  }
+
+  function nowPlaying(){
+    $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
+  }
+
+  function play(){
+    $('#play').on('click', nowPlaying)
+  }
+
+  function nowPausing(){
+    $('#songPaused').html(songToPlay + " is paused");
+  }
+
+  function pause(){
+    $('#pause').on('click', nowPausing);
+  }
+   function showSongs(){
+    var songs = "";
+    for (var key in jukeboxSongs) {
+    songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
+  }
+    $('#showSongs').html(songs);
+  }
+  function show(){
+    $('#show').on('click', showSongs);
+  }
+
