3b2f3245e8ccb83d76e596c0bd99167b8052fd4e
Charles Pletcher <cpletcher2@gmail.com>
2016-03-11 17:16:11 -0500

    Fixes #5
    Rewrite lab so that it's removing anonymous functions, not adding them

--- README.md
+++ README.md
@@ -6,10 +6,10 @@
 
 ## Intro
 
-You've been hired to work for an online Juke Box company. Your job is to clean up their messy code and to replace any defined functions that are only used once with an anonymous function. Remember where you most commonly see anonymous functions, and replace carefully!
+You've been hired to work for an online Juke Box company. Your job is to clean up their messy spaghetti code — it's filled with anonymous functions — into named functions.
 
 ## Instructions
 
-You will be coding your solution in `js/jukebox.js`. You'll want to make sure that things work in both the browser and the tests. The tests currently all pass.You'll want to make sure they continue to pass once you're done refactoring.
+You will be coding your solution in `js/jukebox.js`. You'll want to make sure that things work in both the browser and the tests. The tests currently all pass. You'll want to make sure they continue to pass once you're done refactoring.
 
 <p data-visibility='hidden'>View <a href='https://learn.co/lessons/js-anonymous-functions-lab' title='Juke Box Mania'>Juke Box Mania</a> on Learn.co and start learning to code for free.</p>
--- js/jukebox.js
+++ js/jukebox.js
@@ -1,54 +1,44 @@
 $(document).ready(function(){
-
   menu();
   play();
   pause();
   show();
-
-
 });
 
-  var jukeboxSongs = { "taylor swift": "in the clear", "beyonce": "halo", "drake": "hotline bling"};
-  var artists = Object.keys(jukeboxSongs);
-  var theArtist = artists[Math.floor(Math.random()) * artists.length];
-  var songToPlay = jukeboxSongs[theArtist];
+var jukeboxSongs = { "taylor swift": "in the clear", "beyonce": "halo", "drake": "hotline bling"};
+var artists = Object.keys(jukeboxSongs);
+var theArtist = artists[Math.floor(Math.random()) * artists.length];
+var songToPlay = jukeboxSongs[theArtist];
 
-  function randomNumber() {
-    return Math.floor(Math.random());
-  }
+function randomNumber() {
+  return Math.floor(Math.random());
+}
 
-  function menu(){
-    $('#menu').on('click', menuOptions);
-  }
-
-   function menuOptions() {
+function menu() {
+  $('#menu').on('click', function(e) {
     $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
-  }
+  });
+}
 
-  function nowPlaying(){
+function play() {
+  $('#play').on('click', function(e) {
     $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
     return "now playing " + songToPlay + " by " + theArtist;
-  }
+  });
+}
 
-  function play(){
-    $('#play').on('click', nowPlaying)
-  }
-
-  function nowPausing(){
+function pause() {
+  $('#pause').on('click', function(e) {
     $('#songPaused').html(songToPlay + " is paused");
-  }
+  });
+}
 
-  function pause(){
-    $('#pause').on('click', nowPausing);
-  }
-   function showSongs(){
+function show() {
+  $('#show').on('click', function(e) {
     var songs = "";
     for (var key in jukeboxSongs) {
-    songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
-  }
+      songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
+    }
     $('#showSongs').html(songs);
-  }
-  function show(){
-    $('#show').on('click', showSongs);
-  }
-
+  });
+}
